02babb52831f57e21edbc26bdc6d96b00343e303
Bernardo Sertório <besertorio@gmail.com>
2021-05-20 15:04:18 -0300

    creating header component

---
+++ frontend/src/components/Header/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 60px;
+  a {
+    display: flex; 
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2;
+    &:hover {
+      color: #666;
+    }
+  }
+  h1 {
+    font-size: 20px;
+    margin-left: 10px;
+  }
+`;
