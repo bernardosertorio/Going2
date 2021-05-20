e05cff72468889459c65bb3950247ad0d207e110
Bernardo Sertório <besertorio@gmail.com>
2021-05-20 15:03:29 -0300

    config route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-    <GlobalStyle />
-    </div>
+    <>
+      <Router>
+        <Routes />
+      </Router>
+      <GlobalStyle />
+    </>
   );
 }
 
