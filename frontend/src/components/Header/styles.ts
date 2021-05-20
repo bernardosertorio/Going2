883c3266f9f835f79e38db2968d7ecada3777345
Bernardo Sertório <besertorio@gmail.com>
2021-05-20 16:35:39 -0300

    styling component header

--- frontend/src/components/Header/styles.ts
+++ frontend/src/components/Header/styles.ts
@@ -1,22 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  font-size: 48px;
-  color: #3a3a3a;
-  max-width: 450px;
-  line-height: 56px;
-  margin-top: 60px;
-  a {
-    display: flex; 
-    text-decoration: none;
-    color: #a8a8b3;
-    transition: color 0.2;
-    &:hover {
-      color: #666;
-    }
-  }
+  width: 100%;
+  max-width: 1120px;
+  height: 20rem;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   h1 {
-    font-size: 20px;
-    margin-left: 10px;
+    font-size: 30px;
+    color: #E02041;
   }
+
 `;
