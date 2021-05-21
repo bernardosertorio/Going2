8e4e7e57a0d1a0185623382d18758c010464e84d
Bernardo Sertório <besertorio@gmail.com>
2021-05-21 19:49:20 -0300

    instantiating FormEncodeDecodeUrl in Dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/Header'; 
+import { FormEncodeDecodeUrl } from '../../components/FormEncodeDecodeUrl';
 
 export function Dashboard() {
   return (
     <>
       <Header />
+      <FormEncodeDecodeUrl />
     </>
   )
-}
+};
