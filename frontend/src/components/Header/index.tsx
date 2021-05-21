4d2b1bfabf502722781e7396711079485fed1dfd
Bernardo Sertório <besertorio@gmail.com>
2021-05-21 19:46:53 -0300

    update title

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Container } from './styles';
 export function Header() {
   return (
     <Container>
-      <h1>Encode and Decode Service</h1>
+      <h1>Encode and Decode Url Service</h1>
     </Container>
   )
 }
